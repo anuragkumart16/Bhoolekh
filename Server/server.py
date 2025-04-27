8c19a14183bfdb9803341c0cc0044856a8c6b93e
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-04-27 12:21:43 +0530

    server ready

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,30 @@
 from flask import Flask, request, jsonify
-
+import utils
 app = Flask(__name__)
-@app.route('/hello')
-def hello():
-    return 'hii'
+
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_name():
+    response = jsonify({
+        'location': utils.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict_home_price', methods=['POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+    response = jsonify({
+        'estimated_price': utils.get_estimated_price(location,total_sqft,bath,bhk)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     print('App is running')
+    utils.load_saved_artifacts()
     app.run(debug=True)
 
